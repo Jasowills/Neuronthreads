87b79be27c5ddc029b30e08eb87935c85e314cf9
Jasowills <Jujuzhaddy01@gmail.com>
2024-08-09 09:27:05 +0100

    origin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,19 +3,24 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import axios from 'axios';
-import './main.css'; // Import Tailwind CSS
+import './main.css'; // Import your custom CSS
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+// Create Vue app instance
 const app = createApp(App);
 
+// Set Axios default baseURL from environment variable
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URI;
 
-
-axios.defaults.baseURL = 'http://localhost:3000/api';
+// Set Authorization header if token is present in localStorage
 const token = localStorage.getItem('token');
 if (token) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
+// Use router and store in the app
 app.use(router);
 app.use(store);
+
+// Mount the app to the DOM
 app.mount('#app');
